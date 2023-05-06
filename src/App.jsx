d308b7a6b41a05b0cfe55397668b34f74777ff33
Mukhul Menon <mukhulshankar16@gmail.com>
2023-05-06 13:24:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import React,{useState} from 'react'
+import "./App.css";
+import { Configuration,OpenAIApi } from 'openai';
+import {BeatLoader} from "react-spinners";
+
+const App = () => {
+
+  const [isLoading,setIsLoading]= useState(false);
+
+  const [formData,setFormData] = useState({language:"hindi",message:""});
+ 
+  const[error,setError] = useState("")
+ 
+  const handleInputChange=(e)=>{
+    setFormData({ ...formData, [e.target.name]:e.target.value});
+    setError("")
+  }
+
+  const [translation,setTranslation] = useState("");
+  
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  
+  const translate= async()=>{ 
+    const{language,message}=formData
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Translate this into ${language}: ${message}`,
+    temperature: 0.3,
+    max_tokens: 100,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+const translatedText=response.data.choices[0].text.trim();
+setIsLoading(false);
+setTranslation(translatedText);
+}
+
+  const [showNotification,setShowNotification]= useState(false)
+  
+  const handleCopy=()=>{
+    navigator.clipboard.writeText(translation)
+    .then(()=>displayNotification())
+    .catch((err)=>console.log("failed to copy: ",err));
+  };
+  
+  const displayNotification=()=>{
+  setShowNotification(true);
+  setTimeout(()=>{setShowNotification(false);
+  },3000);
+}
+
+const handleOnSubmit=(e)=>{
+    e.preventDefault();
+  if(!formData.message){
+    setError("Please enter the message")
+    return; 
+  }
+  setIsLoading(true);
+  translate();
+   };
+
+  return (
+    <div className='container'>
+      <h1>Translation</h1>
+      <form onSubmit={handleOnSubmit}>
+        <div className="choices">
+          <input type="radio" id="hindi" value="hindi" name="language" defaultChecked={formData.language} onChange={handleInputChange}/>
+          <label htmlFor="hindi">Hindi</label>
+
+          <input type="radio" id="spanish" value="spanish" name="language" onChange={handleInputChange}/>
+          <label htmlFor="spanish">Spanish</label>
+
+          <input type="radio" id="french" value="french" name="language" onChange={handleInputChange}/>
+          <label htmlFor="french">French</label>
+
+          <input type="radio" id="russian" value="russian" name="language" onChange={handleInputChange}/>
+          <label htmlFor="russian">Russian</label>
+
+          <input type="radio" id="japanese" value="japanese" name="language" onChange={handleInputChange}/>
+          <label htmlFor="japanese">Japanese</label>
+
+          <input type="radio" id="german" value="german" name="language" onChange={handleInputChange}/>
+          <label htmlFor="german">German</label>
+
+          <input type="radio" id="arabic" value="arabic" name="language" onChange={handleInputChange}/>
+          <label htmlFor="arabic">Arabic</label>
+
+          <input type="radio" id="chinese" value="chinese" name="language" onChange={handleInputChange}/>
+          <label htmlFor="chinese">Chinese</label>
+
+          <input type="radio" id="korean" value="korean" name="language" onChange={handleInputChange}/>
+          <label htmlFor="korean">Korean</label>
+        </div>
+        <textarea name="message" placeholder='Type your message here...' onChange={handleInputChange}>
+        </textarea>
+       {error && <div className="error"> {error}</div>}
+        <button type='submit'>Translate</button>
+      </form>
+      <div className="translation">
+        <div className="copybtn" onClick={handleCopy}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+       <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+        </svg>
+        </div>
+        {isLoading ?<BeatLoader size={12} color={"green"}/> :translation}
+      </div>
+      <div className={`notification ${showNotification ? "active" : ""}`}>
+        Copied to clipboard
+      </div>
+    </div>
+  )
+}
+
+export default App
